e8d6e661c95b5202992c539fce789955c667b7eb
Bogdan Barbu <bogdan.barbu@ymail.com>
2022-08-28 14:52:53 +0300

    feat(util): scaled text and screen text bar support

---
+++ src/main/java/rooftopjoe/mcclientpp/util/Constants.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2022 Bogdan Barbu
+ *
+ * This file is part of MC Client++.
+ *
+ * MC Client++ is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * MC Client++ is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * MC Client++. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package rooftopjoe.mcclientpp.util;
+
+public class Constants {
+    public static final int SCREEN_TEXTBAR_COLOR = 0x4F000000;
+}
---
+++ src/main/java/rooftopjoe/mcclientpp/util/TextUtils.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2022 Bogdan Barbu
+ *
+ * This file is part of MC Client++.
+ *
+ * MC Client++ is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * MC Client++ is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * MC Client++. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package rooftopjoe.mcclientpp.util;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+public class TextUtils {
+    public enum Justification {
+        LEFT,
+        CENTER,
+        RIGHT
+    }
+
+    static final MinecraftClient client = MinecraftClient.getInstance();
+
+    public static void drawScaledText(MatrixStack matrices, String text, int x, int y, int maxWidth, Justification justification, int color) {
+        int width = client.textRenderer.getWidth(text), offset = 0;
+        float scaleFactor = width <= maxWidth ? 1 : (float)maxWidth / width;
+
+        switch (justification) {
+            case LEFT: break;
+            case CENTER: if (width <= maxWidth) offset = (maxWidth - width) / 2; break;
+            case RIGHT: if (width <= maxWidth) offset = maxWidth - width; break;
+        }
+
+        matrices.push();
+        matrices.translate(x, y, 300);
+        matrices.scale(scaleFactor, scaleFactor, 1);
+
+        client.textRenderer.drawWithShadow(matrices, text, offset, 0, color);
+
+        matrices.pop();
+    }
+
+    public static void drawScreenTextBar(MatrixStack matrices, String text, int x, int y) {
+        int width = client.textRenderer.getWidth(text);
+
+        DrawableHelper.fill(matrices, x - width - 12, y, x - 8, y + 12, Constants.SCREEN_TEXTBAR_COLOR);
+        client.textRenderer.drawWithShadow(matrices, text, x - width - 10, y + 2, Formatting.WHITE.getColorValue());
+    }
+}
